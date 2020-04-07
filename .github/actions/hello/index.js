69e7f696b7284f63cf32920d90b3069d38c43225
Akshay Dhimnan <akshaydhiman@cloudanalogy.com>
2020-04-07 21:53:35 +0530

    refactor: added hello custom action

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,11 @@
+const core = require("@actions/core");
+
+try {
+  const name = core.getInput("who-to-greet");
+  console.log(`Hello ${name}`);
+
+  const time = new Date();
+  core.setOutput("time", time.toTimeString());
+} catch (error) {
+  core.setFailed(error.message);
+}
